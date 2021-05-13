6e2670087560d91bee4bd4ae9e1ac9df22bea7bc
Eduarda Amaral <eduarda12carmo@hotmail.com>
2021-05-12 22:32:06 -0300

    Criacao do Banco de Dados

---
+++ ProcedureCadastro.sql
@@ -0,0 +1,40 @@
+create database divulgatudo;
+
+use divulgatudo;
+
+create table anuncios(
+	id int not null auto_increment,
+    nome varchar(100) not null,
+    dataInicio date not null,
+    dataFinal date,
+    primary key(id),
+    fk_pessoa int(4) NOT NULL,
+    foreign key(fk_pessoa) references pessoa(id),
+    fk_investimento int(4) NOT NULL,
+    foreign key(fk_investimento) references investimento(id)
+);
+
+create table pessoas(
+	id int not null auto_increment,
+    nome varchar(100) not null,
+    ip varchar(15),
+    primary key(id)
+);
+
+create table investimentos(
+	id int not null auto_increment, 
+    valor numeric(9,2) not null,
+    primary key(id)
+);
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+FLUSH PRIVILEGES;
+
+INSERT INTO pessoa(id, nome, ip)
+VALUES(1,'Eduarda','123.123.123.123');
+
+INSERT INTO investimento(id, valor)
+VALUES(1,1000);
+
+INSERT INTO anuncios(id, nome, dataInicio, dataFinal, fk_pessoa, fk_investimento)
+VALUES (1,'Divulga Tudo','2021-05-12','2021-05-20',1,1);
